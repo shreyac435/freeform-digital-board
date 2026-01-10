8275df496e445f5305c167f97b94a18fe0674125
shreyac <shreya43500@gmail.com>
2026-01-10 14:27:46 +0530

    created board

---
+++ page.tsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#f5f5f5",
+        color: "black",
+        fontSize: "24px",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      Board ready
+    </div>
+  );
+}
+
+
+
