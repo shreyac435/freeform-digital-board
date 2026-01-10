c6d39964a77cd58f8dc918fbae643fdfd4f15f58
shreyac <shreya43500@gmail.com>
2026-01-10 14:47:25 +0530

    made board dynamic size

---
+++ components/canvas.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { Stage, Layer, Rect } from "react-konva";
+import { useEffect, useState } from "react";
+
+export default function Canvas() {
+const [size, setSize] = useState({
+    width: 800,
+    height: 600,
+  });
+
+  useEffect(() => {
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight,
+    });
+  }, []);
+  return (
+    <Stage width={size.width} height={size.height}>
+      <Layer>
+        <Rect
+          x={50}
+          y={50}
+          width={120}
+          height={80}
+          fill="black"
+        />
+      </Layer>
+    </Stage>
+  );
+}
--- page.tsx
+++ page.tsx
@@ -1,21 +1,9 @@
+import Canvas from "./components/canvas";
+
 export default function Home() {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        backgroundColor: "#f5f5f5",
-        color: "black",
-        fontSize: "24px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      Board ready
-    </div>
-  );
+  return <Canvas />;
 }
+
 
 
 
